29a428ad9235eb5f49250ed663a55088d3fa2752
duyguhz <duyghuhh@code.edu.az>
2022-05-30 06:14:57 +0400

    hw

---
+++ Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace homework3_
+{
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public int CurrentFuel;
+        public int FuelFor1Km;
+        public int Millage;
+
+        public int Drive()
+        {
+            Console.WriteLine("getmek istediyimiz km:");
+          
+            
+        }
+    }
+}
